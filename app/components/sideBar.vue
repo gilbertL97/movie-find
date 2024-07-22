091393f019eaa0e8373c65b04c67b4432a52a865
Gilbert Luis <gilbertluis97@gmail.com>
2024-07-22 14:51:43 -0400

    comenzando con el sidemenu

---
+++ app/components/sideBar.vue
@@ -0,0 +1,16 @@
+<template>
+  <nav>
+    <NuxtLink v-slot="{isActive}" class="text-2xl">
+
+    </NuxtLink>
+    
+  </nav>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
---
+++ app/pages/index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+const route = useRoute()
+</script>
+
+<template>
+  <div>
+    <h1>test</h1>
+  </div>
+</template>
